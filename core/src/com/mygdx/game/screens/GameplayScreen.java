7ece69f9a3771f77ede4d2ef8a893270c0aaea51
Demogorgon <paweh10@gmail.com>
2016-07-25 17:04:37 +0200

    Added player class

--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package com.mygdx.game.screens;
 
 import com.mygdx.game.IdleClicker;
+import com.mygdx.game.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
+	
+	private Player player;
 
 	public GameplayScreen(IdleClicker game) {
 		super(game);
@@ -10,7 +13,27 @@ public class GameplayScreen extends AbstractScreen {
 	}
 
 	private void init() {
-		// TODO Auto-generated method stub
+		InitGenerateCashButton();
+		
+	}
+
+	private void InitGenerateCashButton() {
+		player = new Player();
+		stage.addActor(player);
+	}
+	
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		update();
+		
+		spriteBatch.begin();
+		stage.draw();
+		spriteBatch.end();
+	}
+
+	private void update() {
+		stage.act();
 		
 	}
 
