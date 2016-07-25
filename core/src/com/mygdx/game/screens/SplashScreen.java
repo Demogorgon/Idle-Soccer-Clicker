b00b61b67aba8730ce467e3c1f34b89d8081a93d
Demogorgon <Demogorgon>
2016-07-25 15:39:30 +0200

    Change SplashScreen to GameplayScreen after 2 seconds.

--- core/src/com/mygdx/game/screens/SplashScreen.java
+++ core/src/com/mygdx/game/screens/SplashScreen.java
@@ -1,19 +1,30 @@
 package com.mygdx.game.screens;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 import com.mygdx.game.IdleClicker;
 
 public class SplashScreen extends AbstractScreen{
 
 	private Texture splashImg;
 
-	public SplashScreen(IdleClicker game) {
+	public SplashScreen(final IdleClicker game) {
 		super(game);
 		init();
+		
+		Timer.schedule(new Task() {
+			
+			@Override
+			public void run() {
+				game.setScreen(new GameplayScreen(game));
+			}
+		}, 2);
 	}
 
 	private void init() {
 		splashImg = new Texture("ball.png");
+		
 	}
 
 	@Override
