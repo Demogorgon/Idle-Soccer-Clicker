4f93dd50dc97d5e9a470d32ef7b4815923e232ec
Demogorgon <paweh10@gmail.com>
2016-07-26 19:11:49 +0200

    Added Generate cash button and ClickListener

--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -1,11 +1,16 @@
 package com.mygdx.game.screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.IdleClicker;
 import com.mygdx.game.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
 	
 	private Player player;
+	private Button generateCashButton;
 
 	public GameplayScreen(IdleClicker game) {
 		super(game);
@@ -13,12 +18,33 @@ public class GameplayScreen extends AbstractScreen {
 
 	@Override
 	protected void init() {
+		InitPlayer();
 		InitGenerateCashButton();
 	}
 	
-	private void InitGenerateCashButton() {
+	private void InitPlayer() {
 		player = new Player();
 		stage.addActor(player);
+	}
+
+	private void InitGenerateCashButton() {
+		generateCashButton = new Button(new ButtonStyle());
+		generateCashButton.setWidth(460);
+		generateCashButton.setHeight(360);
+		generateCashButton.setX(50);
+		generateCashButton.setY(170);
+		generateCashButton.setDebug(true);
+		
+		stage.addActor(generateCashButton);
+		
+		generateCashButton.addListener(new ClickListener(){
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("Click");
+				return super.touchDown(event, x, y, pointer, button);
+			}
+		});
+		
 	}
 	
 	@Override
