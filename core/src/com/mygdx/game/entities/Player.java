f40acdc5e162f687b23b5deed263b5da575e9cdd
Demogorgon <paweh10@gmail.com>
2016-07-26 19:10:50 +0200

    Player Image size changed

--- core/src/com/mygdx/game/entities/Player.java
+++ core/src/com/mygdx/game/entities/Player.java
@@ -4,8 +4,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class Player extends Image{
-	private final static int WIDTH = 77;
-	private final static int HEIGHT = 152;
+	private final static int WIDTH = 68;
+	private final static int HEIGHT = 70;
 
 	private final static int ButtonPosX = 200;
 	private final static int ButtonPosY = 300;
